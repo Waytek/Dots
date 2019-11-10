1cc445874edeca35def59689babd017c41601ebc
viktor kodolov <vkodolov@tamashi.games>
2019-11-11 02:47:59 +0700

    Create PlayerData

---
+++ Assets/Scripts/Game/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerData
+{
+  public static int Score
+  {
+    get
+    {
+      return PlayerPrefs.GetInt("playerScore", 0);
+    }
+    set
+    {
+      PlayerPrefs.SetInt("playerScore", value);
+    }
+  }
+}
