90dbf3e49d7d5d56132402fc3fcc9c52841b7065
viktor kodolov <vkodolov@tamashi.games>
2019-11-11 02:48:17 +0700

    Crate scroe ui helper

---
+++ Assets/Scripts/Game/UI/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+  [SerializeField]
+  private Text text;
+
+  private void Start()
+  {
+    EventManager.StartListening(EventManager.Events.DotsDestroyed, onDotDestroyed);
+    updateScoreText();
+  }
+
+  private void onDotDestroyed()
+  {
+    updateScoreText();
+  }
+
+  private void updateScoreText()
+  {
+    text.text = PlayerData.Score.ToString();
+  }
+}
