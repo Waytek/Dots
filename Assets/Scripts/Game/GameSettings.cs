b48e26e329b6a77d8c0e928e520443ea2075f58f
viktor kodolov <vkodolov@tamashi.games>
2019-11-11 02:47:39 +0700

    Create global game settings for add scroe from dot

---
+++ Assets/Scripts/Game/GameSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+  private static GameSettings instance;
+  public static GameSettings Instance
+  {
+    get
+    {
+      if (!instance)
+      {
+        instance = FindObjectOfType(typeof(GameSettings)) as GameSettings;
+
+        if (!instance)
+        {
+          instance = new GameObject("GameSettings").AddComponent<GameSettings>();
+        }
+      }
+
+      return instance;
+    }
+  }
+
+  [SerializeField]
+  private int scoreFromDot = 10;
+  public static int ScoreFromDot => Instance.scoreFromDot;
+}
